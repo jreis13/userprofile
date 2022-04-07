f0c0e26fc64e6244cf5c325b5be7a5420f70012e
João Francisco Reis <joaofranciscoreis13@outlook.com>
2022-04-07 23:13:55 +0100

    small fixes

--- src/components/Main/ProfileInformation.js
+++ src/components/Main/ProfileInformation.js
@@ -15,6 +15,48 @@ function ProfileInformation() {
           <div className="progress-bar"></div>
           <div className="reward"></div>
         </li>
+
+        <li className="profile-info-item">
+          <div className="item-top-info">
+            <p className="rank">NFT Collector</p>
+            <p className="level-text">Level 1</p>
+          </div>
+          <div className="description">
+            <p className="alt-text">
+              Collect a total of 25 NFTs in your wallet (minted)
+            </p>
+          </div>
+          <div className="progress-bar"></div>
+          <div className="reward"></div>
+        </li>
+
+        <li className="profile-info-item">
+          <div className="item-top-info">
+            <p className="rank">NFT Collector</p>
+            <p className="level-text">Level 1</p>
+          </div>
+          <div className="description">
+            <p className="alt-text">
+              Collect a total of 25 NFTs in your wallet (minted)
+            </p>
+          </div>
+          <div className="progress-bar"></div>
+          <div className="reward"></div>
+        </li>
+
+        <li className="profile-info-item">
+          <div className="item-top-info">
+            <p className="rank">NFT Collector</p>
+            <p className="level-text">Level 1</p>
+          </div>
+          <div className="description">
+            <p className="alt-text">
+              Collect a total of 25 NFTs in your wallet (minted)
+            </p>
+          </div>
+          <div className="progress-bar"></div>
+          <div className="reward"></div>
+        </li>
       </ul>
     </div>
   );
